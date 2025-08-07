db71e0e224baeaf9d6053cf5040e0b39c17cc04c
Jirkis69 <jirkisp@seznam.cz>
2025-08-07 12:51:47 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v4';
+const CACHE_VERSION = 'v5';
 const CACHE_NAME = `kubb-cache-${CACHE_VERSION}`;
 const FILES_TO_CACHE = [
   '/',
@@ -77,3 +77,4 @@ self.addEventListener('message', event => {
     self.skipWaiting();
   }
 });
+
